f893d3b287ebb9fe76e4480c57af71fe9fdbce16
Virginia Dooley <vcdooley@gmail.com>
2019-08-27 11:13:30 +0100

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,29 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
 
-}
+    const restaurant = {
+        id: cuidFn(),
+        text: action.text
+      }
+      return  {
+        ...state, restaurants:
+        [ ...state.restaurants, restaurant]
+      }
+
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant =>
+        restaurant.id !== action.id)
+      return {
+        ...state, restaurants
+      }
+
+    default:
+      return state;
+  }
+};
